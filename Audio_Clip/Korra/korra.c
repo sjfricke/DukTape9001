f93df42553af36c72c2221c8c55f448c04a32a43
sjfricke <spencerfricke@gmail.com>
2018-02-18 07:16:46 +0000

    still failing audio

--- Audio_Clip/Korra/korra.c
+++ Audio_Clip/Korra/korra.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdint.h>
 #include <string.h>
 #include <ctype.h>
@@ -8,9 +9,9 @@
 
 #define PS_BUF_SIZE 4096
 
-ps_decoder_t *ps;
-cmd_ln_t *config;
-ad_rec_t *ad; // Audio Device
+static ps_decoder_t *ps;
+static cmd_ln_t *config;
+static ad_rec_t *ad; // Audio Device
 int16_t buffer[PS_BUF_SIZE];
 int32_t n_samples;
 uint8_t utt_started = 0;
@@ -18,16 +19,13 @@ uint8_t in_speech;
 char const* phrase;
 char const* output;
 char sentence[100];
-char fileNames[10][20];
-// forward declaration
-void Add(char const* phrase);
 
 void DictionarySetup() {
 
   // Create config object with path to language, .lm file, and .dic file
   config = cmd_ln_init(NULL, ps_args(), TRUE,
                        "-hmm", "/usr/local/share/pocketsphinx/model/en-us/en-us"
-,
+		       ,
                        "-lm", "./korra.lm",
                        "-dict", "./korra.dic",
                        NULL);
@@ -43,33 +41,52 @@ void DictionarySetup() {
     fprintf(stderr, "Failed to create recognizer, see log for details\n");
     exit(-1);
   }
-}
-
-void GetPhrase() {
-
   // plughw:0,2 is capture on dragonboard by default
   if ((ad = ad_open_dev("plughw:0,2", 16000)) == NULL) {
     fprintf(stderr, "Failed to open audio device\n");
     exit(-1);
   }
+  
+}
+
+void GetPhrase() {
+
+  	
+  //Processing information using the phrase
+  char sentence[1024];
+  char cstring[1024];
+  const char wav[9] = ".wav -i";
+  char commandString[1024];
+  strcpy(commandString, "ffmpeg -i ");
+  //This is the new array of strings which is used to hold the words.
+  int i,j,k,cctr,sInt,wctr;
+  int l;
+  char minichar[6];
+  char anotherMini[11];
+  int starting = 0;
+  
+ 
+  printf("\nSAY YOUR PHRASE AVATAR...\n\n");
 
   // start recording
   if (ad_start_rec(ad) < 0) {
     fprintf(stderr, "Failed to start recording\n");
     return;
   }
-
+  
   // start utterance
   if (ps_start_utt(ps) < 0) {
     fprintf(stderr, "Failed to start utterance\n");
     return;
   }
 
-  printf("\nSAY YOUR PHRASE AVATAR...\n\n");
-
   // Loop until word is fetched
   while(1) {
-
+    if (starting == 0){
+      starting = 1;
+      
+    }
+    
     // Start getting data
     if ((n_samples = ad_read(ad, buffer, PS_BUF_SIZE)) < 0) {
       fprintf(stderr, "Failed to read audio\n");
@@ -87,21 +104,87 @@ void GetPhrase() {
     if (!in_speech && utt_started) {
       ps_end_utt(ps);
       phrase = ps_get_hyp(ps, NULL);
-	  strcpy(sentence,phrase);
-/*       if(phrase == NULL){
+      strcpy(sentence,phrase);
+      if(phrase == NULL){
         fprintf(stderr, "Phrase was null\n");
         ad_close(ad);
         return;
+      } else {
+	// good phrase
+ 	ps_end_utt(ps);
+	printf("---------------------------------------\n");    
+	
+	for(k=0; phrase[k]!='\0';k++){
+	  sentence[k]=toupper(phrase[k]);
+	}
+	
+	j=0; cctr=0; sInt = 0; wctr = 0;
+	for(i=0;i<=(strlen(sentence));i++)
+	  {
+	    // if space or NULL found, assign NULL into newString[ctr]
+	    if(sentence[i]==' '||sentence[i]=='\0')
+	      {
+		//This copies from the base string 
+		if(i == strlen(sentence)){
+		  memcpy(cstring+cctr,sentence+sInt,j);
+		  cctr = cctr +j;//Changes the index for the insertion
+		  memcpy(cstring+cctr,wav,5);
+		  cctr = cctr + 5;
+		}
+		else{
+		  memcpy(cstring+cctr,sentence+sInt,j);
+		  cctr = cctr +j;//Changes the index for the insertion
+		  memcpy(cstring+cctr,wav,7);
+		  cctr = cctr + 7;
+		}
+		sInt = sInt + j;
+		j=1;
+		wctr++;
+	      }
+	    else
+	      {
+		j++;
+	      }
+	  }
+    	
+	//Finishing cstring and preparing it to be parsed as command string.
+	cstring[cctr]='\0';
+	printf(cstring);
+	printf("\n Strings or words after split by space are :\n");
+	//printf(cstring);
+	strcat(commandString,cstring);
+	strcat(commandString, "-filter_complex \"");
+	
+	//Mini loop used to insert the [o:o] . . . command parameters
+	for(l = 0; l < wctr; l++){
+	  sprintf(minichar, "[%i:0]",l);
+	  strcat(commandString, minichar);
+		
+	}
+	sprintf(anotherMini,"concat=n=%i",wctr);
+	strcat(commandString,anotherMini);
+	strcat(commandString,":v=0:a=1[outa]\" -map \"[outa]\" output.wav");
+	system(commandString);
+	system("aplay -D plughw:0,1 output.wav");
+	system("rm output.wav");
+	printf("\n\n");
+	printf(commandString);
+	printf("\n");
+	printf(phrase);
+	starting = 0;
+	ps_start_utt(ps);
+	printf("\n\n"); 
       }
 
       if (ps_start_utt(ps) < 0) {
-        fprintf(stderr, "Failed to start utterance\n");
-        ad_close(ad);
-        return;
-      } */
-          return;
+        fprintf(stderr, "Failed to start utterance at end of loop\n");
+	 ad_close(ad);
+	 return;
+      }
+      usleep(100000); //100ms
     }
-  } // while(1)
+  }
+	
 }
 
 void CleanUp() {
@@ -112,91 +195,17 @@ void CleanUp() {
 
 int main(int argc, char* argv[]) {
 
+  err_set_logfile("log.txt");  
+  
   DictionarySetup();
-  while(1){
+  printf("\n\nITS STILL WORKING HERE\n\n");
+ 
   GetPhrase();
-  	//This is the string that is getting split
-    char sentence[1024];
-	char cstring[1024];
-	const char wav[9] = ".wav -i";
-	char commandString[1024];
-	strcpy(commandString, "ffmpeg -i ");
-	printf("\n\n");
-	printf(phrase);
-	printf("\n\n");
-	//This is the new array of strings which is used to hold the words.
-    int i,j,k,cctr,sInt,wctr;
-       printf("\n\n Split string by space into words :\n");
-       printf("---------------------------------------\n");    
-	for(k=0; phrase[k]!='\0';k++){
-		sentence[k]=toupper(phrase[k]);
-	}
-    j=0; cctr=0; sInt = 0; wctr = 0;
-    for(i=0;i<=(strlen(sentence));i++)
-    {
-        // if space or NULL found, assign NULL into newString[ctr]
-        if(sentence[i]==' '||sentence[i]=='\0')
-        {
-			//This copies from the base string 
-			if(i == strlen(sentence)){
-				memcpy(cstring+cctr,sentence+sInt,j);
-				cctr = cctr +j;//Changes the index for the insertion
-				memcpy(cstring+cctr,wav,5);
-				cctr = cctr + 5;
-			}
-			else{
-				memcpy(cstring+cctr,sentence+sInt,j);
-				cctr = cctr +j;//Changes the index for the insertion
-				memcpy(cstring+cctr,wav,7);
-				cctr = cctr + 7;
-			}
-			sInt = sInt + j;
-			//printf("\nsInt = %i\n",sInt);
-			//printf(cstring);
-            j=1;    //for next word, init index to 0
-			wctr++;
-        }
-        else
-        {
-            //newString[ctr][j]=sentence[i];
-            j++;
-        }
-	}
+  //This is the string that is getting split
     
-	//cstring[cctr] = '\0';
-	cstring[cctr]='\0';
-	printf("\n Strings or words after split by space are :\n");
-	//printf(cstring);
-	strcat(commandString,cstring);
-	strcat(commandString, "-filter_complex \"");
-	//printf(commandString);
-	//printf(commandString);
-	int l;
+  CleanUp();
 	
-	for(l = 0; l < wctr; l++){
-		char minichar[6];
-		sprintf(minichar, "[%i:0]",l);
-		strcat(commandString, minichar);
-		
-	}
-	char anotherMini[11];
-	sprintf(anotherMini,"concat=n=%i",wctr);
-	strcat(commandString,anotherMini);
-	strcat(commandString,":v=0:a=1[outa]\" -map \"[outa]\" output.wav");
-	system(commandString);
-	system("aplay -D plughw:0,1 output.wav");
-	system("rm output.wav");
-	printf("\n\n");
-	printf(commandString);
-	printf("\n");
-	//printf(commandString);
-    printf("\n");
-	printf(phrase);
-	printf("\n\n");
-
-	CleanUp();
-	}
-	return 0;
+  return 0;
 
 	  
 }
